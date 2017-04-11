385d733eb22577ca7105f8d6528ca2b99f935058
Zhongjian Lv <568066242@qq.com>
2017-04-11 09:47:25 +0800

    finish 16

---
+++ src/ThreeSumClosest16.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Created by zhongjianlv on 17-4-10.
+ * <p>
+ * leetcode algorithm 16
+ * <p>
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ * <p>
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+public class ThreeSumClosest16 {
+
+
+    public int threeSumClosest(int[] nums, int target) {
+        int res = nums[0] + nums[1] + nums[2];
+        int old_abs = Math.abs(res - target);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]){
+                int lo = i + 1;
+                int hi = nums.length - 1;
+                int new_target = target - nums[i];
+                while (lo < hi){
+                    if(Math.abs(new_target - nums[lo] - nums[hi]) < old_abs){
+                        old_abs = Math.abs(new_target - nums[lo] - nums[hi]);
+                        res = nums[i] + nums[hi] + nums[lo];
+                    }
+                    if(nums[lo] + nums[hi] == new_target){
+                        return target;
+                    }
+                    else if (nums[lo] + nums[hi] < new_target) {
+                        lo++;
+                    }
+                    else {
+                        hi--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+    /**
+     * O(n^3) time limited
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest1(int[] nums, int target) {
+        int res = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int r = nums[i] + nums[j] + nums[k];
+                    if (Math.abs(target - r) < Math.abs(target - res))
+                        res = r;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ThreeSumClosest16 threeSumClosest16 = new ThreeSumClosest16();
+        System.out.println(threeSumClosest16.threeSumClosest(new int[]{0,2,1,-3},1));
+    }
+
+}
