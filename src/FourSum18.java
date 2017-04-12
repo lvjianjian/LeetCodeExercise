970bad583bb3a4da9acd1123a8ab3a0bb1a28214
Zhongjian Lv <568066242@qq.com>
2017-04-12 18:19:00 +0800

    a

---
+++ src/FourSum18.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by zhongjianlv on 17-4-11.
+ * <p>
+ * leetcode algorithm 18
+ * <p>
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+ * Find all unique quadruplets in the array which gives the sum of target
+ * <p>
+ * Note: The solution set must not contain duplicate quadruplets.
+ * <p>
+ * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+ * <p>
+ * A solution set is:
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ */
+public class FourSum18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+
+
+        return res;
+    }
+
+}
