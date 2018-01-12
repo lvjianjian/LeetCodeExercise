181097925ac052e69dc8f3174a57487a7fedf2cd
Zhongjian Lv <568066242@qq.com>
2018-01-12 20:13:51 +0800

    add 41

--- src/First_Missing_Positive_41.java
+++ src/First_Missing_Positive_41.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 /**
  * Created by zhongjianlv on 18-1-8.
+ *
+ *
  */
 public class First_Missing_Positive_41 {
+
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        int l = nums.length;
+        while(i < l){
+            if(nums[i] == i + 1 || nums[i] <= 0 || nums[i] > nums.length || nums[nums[i] - 1] == nums[i]){
+                ++i;
+            }else{
+                int k = nums[i] - 1;
+                nums[k] ^= nums[i];
+                nums[i] ^= nums[k];
+                nums[k] ^= nums[i];
+            }
+        }
+
+        i = 0;
+        while(i < l){
+            if (nums[i] != i+1)
+                break;
+            ++i;
+        }
+        return i+1;
+    }
+
+
 }
