14b3432530f592cb450dcccb0bbdbcf07024300b
Zhongjian Lv <568066242@qq.com>
2018-03-14 21:42:24 +0800

    add comment

--- src/Random_Pick_Index_398.java
+++ src/Random_Pick_Index_398.java
@@ -23,7 +23,9 @@ public class Random_Pick_Index_398 {
 
     private Map<Integer, List<Integer>> map;
     private Random random;
+    private int[] nums;
     public Random_Pick_Index_398(int[] nums) {
+        this.nums = nums;
         map = new HashMap<>();
         random = new Random();
         for (int i = 0; i < nums.length; i++) {
@@ -37,7 +39,24 @@ public class Random_Pick_Index_398 {
         }
     }
 
+    //对于第i个target, 选择它进行替换的概率是1/i,
+    // 假设对于前面时刻的选择概率都是1/(i-1),
+    // 不替换掉前面的某个的概率是(i-1)/i
+    // 因此当前时刻选择前面的选择概率为1/(i-1) * (i-1)/i = 1/i
     public int pick(int target) {
+        int result = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target)
+                continue;
+            if (random.nextInt(++count) == 0)
+                result = i;
+        }
+
+        return result;
+    }
+
+    public int pick2(int target) {
         List<Integer> list = map.get(target);
         int size = list.size();
         return list.get(random.nextInt(size));
